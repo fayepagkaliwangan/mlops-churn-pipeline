d0b65c1c52ffd68e6c6b6ff46c743c93504a62d0
ela-sarah <b00818625@essec.edu>
2026-04-04 23:38:18 +0200

    Clean up artifacts, ignore logs and models, finalize API

--- api/app.py
+++ api/app.py
@@ -1,12 +1,34 @@
+import joblib
+import pandas as pd
 from fastapi import FastAPI
 from pydantic import BaseModel
 
 app = FastAPI()
 
+#load the correct trained model
+model = joblib.load("models/RandomForest_model.pkl")
+print(f"Loaded model: {type(model)}")
+
 class CustomerInput(BaseModel):
+    gender: str
+    SeniorCitizen: int
+    Partner: str
+    Dependents: str
     tenure: int
-    monthly_charges: float
-    contract: str
+    PhoneService: str
+    MultipleLines: str
+    InternetService: str
+    OnlineSecurity: str
+    OnlineBackup: str
+    DeviceProtection: str
+    TechSupport: str
+    StreamingTV: str
+    StreamingMovies: str
+    Contract: str
+    PaperlessBilling: str
+    PaymentMethod: str
+    MonthlyCharges: float
+    TotalCharges: float
 
 @app.get("/")
 def health():
@@ -14,4 +36,18 @@ def health():
 
 @app.post("/predict")
 def predict(data: CustomerInput):
-    return {"prediction": "yes", "churn_probability": 0.78}
+    try:
+        df = pd.DataFrame([data.dict()])
+        df = pd.get_dummies(df)
+        df = df.reindex(columns=model.feature_names_in_, fill_value=0)
+
+        for col in model.feature_names_in_:
+            if col not in df.columns:
+                df[col] = 0
+        df = df[model.feature_names_in_]
+        prediction = model.predict(df)[0]
+        probability = model.predict_proba(df)[0][1]
+        return {"prediction": "Churn" if prediction == 1 else "No churn","churn_probability": float(probability)}
+
+    except Exception as e:
+        return {"error": str(e)}
